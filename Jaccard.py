affdcc6ca4aaf56fe7db8526a780827fb387653b
Volks24 <46302053+Volks24@users.noreply.github.com>
2019-10-02 19:36:26 -0300

    Jaccard Funcionando Single y Multi - Testear

---
+++ Jaccard.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+from time import time
+
+def jaccard_similarity(x,y):
+	# Compara Posicion a Posicion y devuelve el ratio entre True y el total de posiciones
+	comparacion = np.char.equal(x,y)
+	Pares_Identicos = np.sum(comparacion)
+	return (Pares_Identicos/(len(comparacion)))
+
+t1 = time()
+
+Matriz_Datos = pd.read_excel (r'Datos/Incognita_lite.xlsx', sheet_name='All data_789' )
+
+Jaccard_Matriz = pd.DataFrame(index=Matriz_Datos.columns[1:], columns = Matriz_Datos.columns[1:])
+Indice_Jaccard = []
+	
+
+
+for x in range(1,(Matriz_Datos.shape[1]-1)):
+	indice_col2 = x + 1
+	for y in range (indice_col2,Matriz_Datos.shape[1]):
+		indice_J =jaccard_similarity(Matriz_Datos.iloc[:,x].tolist(), Matriz_Datos.iloc[:,y].tolist()) # [: (Toda la coluna) , Columna ]
+		Indice_Jaccard.append(indice_J) # Valor de similitud Para Cada Par
+		Jaccard_Matriz.loc[Matriz_Datos.columns.values[x], Matriz_Datos.columns.values[y]] = indice_J
+		
+Jaccard_Matriz.fillna("-" , inplace = True)
+Jaccard_Matriz.to_excel("Distancias_Jaccard.xlsx")
+del Jaccard_Matriz
+
+print (time() - t1)
--- Multi.py
+++ Multi.py
@@ -2,23 +2,69 @@ import multiprocessing as mp
 import pandas as pd
 import numpy as np
 
-def jaccard_similarity(x,y,data):
+def jaccard_similarity(x,y , v_1 , v_2):
 	# Compara Posicion a Posicion y devuelve el ratio entre True y el total de posiciones
-	vector_1 = data.iloc[:,x].tolist()
-	vector_2 = data.iloc[:,y].tolist()
-	comparacion = np.char.equal(vector_1,vector_2)
+	comparacion = np.char.equal(x,y)
 	Pares_Identicos = np.sum(comparacion)
-	return (Pares_Identicos/(len(comparacion)))
+	return ((Pares_Identicos/(len(comparacion))), v_1 , v_2)
+	
 
-Datos = pd.read_excel (r'Datos/Incognita_lite.xlsx', sheet_name='All data_789' )
+t1 = time()
 
 
-#print (Datos.shape[0])
-#print (Datos.shape[1]) ### Columnas
+Matriz_Datos = pd.read_excel (r'Datos/Incognita_lite.xlsx', sheet_name='All data_789' )
 
-pool = mp.Pool(processes=4)
 
-results = [pool.apply(jaccard_similarity, args=(Datos.iloc[:,z],Datos.iloc[:,z+1],Datos)) for z in range(0,10)]
 
-print(results)
 
+### Preparacion Datos
+
+Vectores = []
+
+for j in range (0,Matriz_Datos.shape[1]):
+	Vectores.append(Matriz_Datos.iloc[:,j].tolist())
+
+Jaccard_Matriz = pd.DataFrame(index=Matriz_Datos.columns[1:], columns = Matriz_Datos.columns[1:])
+
+
+
+
+# Step 1: Init multiprocessing.Pool()
+pool = mp.Pool(mp.cpu_count())
+
+# Step 2: `pool.apply` the `howmany_within_range()`
+
+resultado_f = []
+
+for v_1 in range(0,Matriz_Datos.shape[1]-1):
+  v_2 = v_1 + 1
+  results = [pool.apply(jaccard_similarity, args=(Vectores[v_1], Vectores[j] ,v_1 , j ) ) for j in range(v_2,Matriz_Datos.shape[1])]
+  for k in range(0,len(results)):
+    Jaccard_Matriz.loc[results[k][2] , results[k][1]] = results[k][0]
+  resultado_f.append(results) # Borrar
+
+		
+
+# Step 3: Don't forget to close
+pool.close()   
+
+Jaccard_Matriz.fillna("-" , inplace = True)
+Jaccard_Matriz.to_excel("Distancias_Jaccard.xlsx")
+
+
+print (time() - t1)
+
+
+combinaciones = 0
+for rows in resultado_f:
+	print(len(rows))
+	combinaciones = combinaciones + len(rows)
+	print("\n")
+
+# print(combinaciones)
+# print(results)
+# print(results[0])
+# print(results[0][0])
+# print(results[0][1][0])
+
+# print (resultado_f[0])
